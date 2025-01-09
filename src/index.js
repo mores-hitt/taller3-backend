e22bc6d13bb0fbe4c4b26f84eafa4be81e4b416b
mores-hitt <fernando.sanabria@alumnos.ucn.cl>
2025-01-09 19:25:38 -0300

    chore: add test log

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import app from "./app";
 const main = () => {
   app.listen(app.get("port"));
   console.log("Server on port", app.get("port"));
+  console.log("TEST DESPLIEGUE SIN ACTIONS");
 };
 
 main();
